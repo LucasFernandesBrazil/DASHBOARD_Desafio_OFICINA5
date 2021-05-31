5ed9919f1855264536fe062acd715b57cc024d6a
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2021-05-31 00:37:05 -0300

    NavBarTop: Ajustes em alinhamento e distribuição

--- JS/button.js
+++ JS/button.js
@@ -1,13 +1,13 @@
 function sideBar_recall (){
     var sideBar = document.getElementById("Sidebar")
 
-    if (sideBar.style.width === "12%") {
+    if (sideBar.style.width === "11%") {
         document.getElementById("Sidebar").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
     }
 
     else {
-        document.getElementById("Sidebar").style.width = "12%";
-        document.getElementById("main").style.marginLeft = "12%";
+        document.getElementById("Sidebar").style.width = "11%";
+        document.getElementById("main").style.marginLeft = "11%";
     }
 }
