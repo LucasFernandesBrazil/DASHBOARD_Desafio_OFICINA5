37be3bf68305e43e21a9b179a6a521ba04909d7b
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2021-06-01 00:56:21 -0300

    Modal: Estruturação básica

--- JS/button.js
+++ JS/button.js
@@ -2,8 +2,8 @@ function sideBar_recall (){
     var sideBar = document.getElementById("Sidebar")
 
     if (sideBar.style.width === "11%") {
-        document.getElementById("Sidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("Sidebar").style.width = "2%";
+        document.getElementById("main").style.marginLeft = "2%";
     }
 
     else {
